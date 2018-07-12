d615b0f5f18f2a2597a06fbb8c2e181099b5758d
Bin Wang <851536944wb@gmail.com>
2018-07-12 16:11:15 +0800

    Update validate_imagenet.py

--- pytorch/validate_imagenet.py
+++ pytorch/validate_imagenet.py
@@ -160,12 +160,6 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
 
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
